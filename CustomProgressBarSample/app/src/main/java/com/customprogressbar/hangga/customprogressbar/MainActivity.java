fcb236cda0c6d3c2e71d08fdab8bb194c5281f8e
sayekti <sayekti@sebangsa.com>
2016-01-07 14:52:19 +0700

    custom progressbar design

---
+++ CustomProgressBarSample/app/src/main/java/com/customprogressbar/hangga/customprogressbar/MainActivity.java
@@ -0,0 +1,13 @@
+package com.customprogressbar.hangga.customprogressbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
